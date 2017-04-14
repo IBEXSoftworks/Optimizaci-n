866781cb0e9c9452427df3f4e299e7a30c658aa8
san <serchgabriel97@gmail.om>
2017-04-13 20:20:19 -0500

    nueva estructura

--- app/main.aot.ts
+++
@@ -1,4 +0,0 @@
-import { platformNativeScript } from "nativescript-angular/platform-static";
-import { AppModuleNgFactory } from "./pages/app.module.ngfactory";
-
-platformNativeScript().bootstrapModuleFactory(AppModuleNgFactory);
--- platforms/android/build/intermediates/assets/F0/debug/app/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { AppModule } from "./pages/app.module";
-
-platformNativeScriptDynamic().bootstrapModule(AppModule);
