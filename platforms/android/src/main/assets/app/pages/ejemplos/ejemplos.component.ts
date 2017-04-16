3456f3a5ae4f6d341057e9f50050c51b830da6a8
U-DESKTOP-RI82C4L\honte <ohernandezs1300@alumno.ipn.mx>
2017-04-16 04:07:38 -0500

    Theory Design

---
+++ platforms/android/src/main/assets/app/pages/ejemplos/ejemplos.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "examples",
+    templateUrl: "pages/ejemplos/ejemplos.component.html",
+    styleUrls: ["pages/ejemplos/ejemplos.component.css"]
+})
+
+export class ExamplesComponent
+{
+    // ExamplesComponent logic
+}
